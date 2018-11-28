b4784c81ea8d5e60edc06799d6bb710ebae8c746
Punnadit Trussat <punnadittr@gmail.com>
2018-11-28 13:56:15 +0700

    Updates

---
+++ precache-manifest.323d838e95fcc090be1707624b05b1b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bb3780ea9d5d95aa1e3",
+    "url": "/react-library/static/js/runtime~main.8bb3780e.js"
+  },
+  {
+    "revision": "556e71a722138df8f066",
+    "url": "/react-library/static/js/main.556e71a7.chunk.js"
+  },
+  {
+    "revision": "7c8bbdbb99a308e5e3b2",
+    "url": "/react-library/static/js/1.7c8bbdbb.chunk.js"
+  },
+  {
+    "revision": "556e71a722138df8f066",
+    "url": "/react-library/static/css/main.fb7223f6.chunk.css"
+  },
+  {
+    "revision": "9e2a849f11d7b9941ca8e3d0e7ce1ba1",
+    "url": "/react-library/index.html"
+  }
+];
